f94ea4bb98db3316ea7664b7785b5e70e6ee53d6
Tom <github@block-net.de>
2024-06-15 21:08:21 +0200

    neu strukturiert

--- extrahiereIbkrSteuerDaten.py
+++ extrahiereIbkrSteuerDaten.py
@@ -1,175 +1,22 @@
 '''
 todos
-Call kauf
-put kauf
+Call kauf Code:C
+put kauf  Code:C
 Aktien kauf
 Aktien verkauf
-gekaufte Aktien verkauf mit puts
 umrechnen in euro
 '''
-
 import pandas as pd
 import argparse
-import csv
-import re
+import globals
 
+import alignCsv as al
+import csvLoader as db
+import filter
+import display as dp
 #--------------------------------------------------------------------------------------------------------------------
-debug = False
 #--------------------------------------------------------------------------------------------------------------------
-def getEncoding(filename:str) -> str:
-    import chardet
-    with open(filename, 'rb') as f:  # Öffnen im Binärmodus
-        rawdata = f.read()
-    result = chardet.detect(rawdata)
-    encoding = result['encoding']
-    print("Encoding = ", encoding)
-    return encoding
-#--------------------------------------------------------------------------------------------------------------------
-pattern = r',(?=(?:[^"]*"[^"]*")*[^"]*$)'
-def count_delimiters(text:str, s:str=","):
-    matches = re.findall(pattern, text)
-    nrs = len(matches)
-    return nrs
-#--------------------------------------------------------------------------------------------------------------------
-def alignSeparators(file_path, output_file, encoding):
-    with open(file_path, 'r', encoding=encoding) as file:
-        lines = file.readlines()
-
-    max_separators = max(count_delimiters(line) for line in lines)
-    # Jede Zeile auf die maximale Anzahl an Separatoren auffüllen
-    aligned_lines = []
-    for line in lines:
-        current_separators = count_delimiters(line)
-        aligned_lines.append(line.strip() + ',' * (max_separators - current_separators) + '\n')
-    with open(output_file, 'w', encoding='utf-8') as file:
-        file.writelines(aligned_lines)
-    return output_file
-#--------------------------------------------------------------------------------------------------------------------
-class MyCustomError(Exception):
-    pass
-#--------------------------------------------------------------------------------------------------------------------
-class CSVTableProcessor:
-    def __init__(self, file_path, delimiter, decimal, encoding):
-        self.file_path = file_path
-        self.encoding = encoding
-        self.delimiter = delimiter
-        self.name = ""
-        self.result = None
-        try:
-            self.data = pd.read_csv(file_path, delimiter=delimiter, decimal=decimal,
-                                     encoding=encoding, header=None, quotechar='"',  encoding_errors='replace')
-        except pd.errors.ParserError as e:
-            print(f"Error reading CSV file: {e}")
-            self.data = None
-            exit()
-        self.tables = self._split_into_tables()
-    #-------------------------------------------------------------------------------------------------
-    def _split_into_tables(self):
-        """Teilt die Daten in separate Tabellen auf, basierend auf den Werten in Spalte A und B."""
-        tables = {}
-        current_table_name = None
-        current_table_data = []
-
-        for index, row in self.data.iterrows():
-            table_name = row.iloc[0]
-            row_type = row.iloc[1]
-
-            if pd.notna(table_name) and row_type == 'Header':
-                if current_table_name is not None and current_table_data:
-                    # Speichern der vorherigen Tabelle
-                    tables[current_table_name] = pd.DataFrame(current_table_data[1:], columns=current_table_data[0])
-                current_table_name = table_name
-                current_table_data = [row[2:].tolist()]
-            elif row_type == 'Data':
-                current_table_data.append(row[2:].tolist())
-            #print(f"Processing row {index}: table_name={table_name}, row_type={row_type}")
-        # Speichern der letzten Tabelle
-        if current_table_name is not None and current_table_data:
-            tables[current_table_name] = pd.DataFrame(current_table_data[1:], columns=current_table_data[0])
-
-        return tables
-    #-------------------------------------------------------------------------------------------------
-    def get_table(self, table_name):
-        """Gibt die Tabelle mit dem angegebenen Namen zurück."""
-        return self.tables.get(table_name)
-    #-------------------------------------------------------------------------------------------------
-    def getRowsOfColumnsContainingStr(self, table, col:str, str:str):
-        return table[table[col].str.contains(str, na=False)]
-    #-------------------------------------------------------------------------------------------------
-    def extractSold(self):
-        table = self.get_table('Transaktionen')
-        return self.getRowsOfColumnsContainingStr(table, "Code", "O")
-    #-------------------------------------------------------------------------------------------------
-    def extractExecutedShorts(self):
-        table = self.get_table('Transaktionen')
-        self.executed = self.getRowsOfColumnsContainingStr(table, "Code", "A.*")
-        return self.executed
-    #-------------------------------------------------------------------------------------------------
-    def splitSymbolExecutedShorts(self,table):
-        split_columns = table['Symbol'].str.split(' ', expand=True)
-        split_columns.columns = ['Ticker', 'Bis', 'Preis', 'Typ']
-        table = table.drop(columns=['Symbol']).join(split_columns)
-        table = table.drop_duplicates(keep=False)
-        return table[["Datum/Zeit", 'Ticker', 'Bis', 'Preis', 'Menge']]   
-    #-------------------------------------------------------------------------------------------------
-    def getSoldOptions(self):
-        df = self.extractSold()
-        df = self.getRowsOfColumnsContainingStr(df, "Symbol", ".*?[CP]$")
-        cols1 = ["Erlös","Prov./Gebühr"]
-        for i in cols1:
-            df[i] = pd.to_numeric(df[i])
-        cols = ["Datum/Zeit", "Symbol"] + cols1
-        str = df[cols].to_string() + "\n" + df[cols1].sum().to_string()
-        print(str)
-    #-------------------------------------------------------------------------------------------------
-    def getExecutedCalls(self):
-        calls = self.getRowsOfColumnsContainingStr(self.executed, "Symbol", ".*?C$")
-        calls = self.splitSymbolExecutedShorts(calls)
-        return calls[["Datum/Zeit", 'Ticker', 'Bis', 'Preis', 'Menge',]]   
-    #-------------------------------------------------------------------------------------------------
-    def getExecutedPuts(self):
-        puts = self.getRowsOfColumnsContainingStr(self.executed, "Symbol", ".*?P$")
-        puts = self.splitSymbolExecutedShorts(puts)
-        return puts[["Datum/Zeit", 'Ticker', 'Bis', 'Preis', 'Menge',]]   
-    #-------------------------------------------------------------------------------------------------
-    def display_tables(self):
-        """Zeigt alle Tabellen an."""
-        for name, table in self.tables.items():
-            print(f"Tabelle: {name}")
-            print(table)
-            print("\n")
-    #-------------------------------------------------------------------------------------------------
-    def filterTable(self, table_name, headerColumnName, search,range):
-        self.result = None
-        table = self.get_table(table_name)
-        if table is None:
-            raise MyCustomError(f"Tabelle '{table_name}' error.")
-        else:
-            filtered_table = table[table[headerColumnName].str.contains(search, na=False)]
-            filtered_table = filtered_table.iloc[:, 0:range]
-            self.result = filtered_table
-            self.name = table_name
-        return self.result
-    #-------------------------------------------------------------------------------------------------
-    def getNameResult(self):
-        return [self.name, self.result]
-    #-------------------------------------------------------------------------------------------------
-    def delCols(self,cols):
-        for col in cols:
-            del self.result[col]
-    #-------------------------------------------------------------------------------------------------
-    def displayLastExecutionResult(self,col:str=None, filter:str = None):
-        try:
-            print(f"Tabelle: {self.name}")
-            t = self.result
-            if col is None:
-                print(t.to_string(na_rep='-'))  # Gibt die gefilterte Tabelle aus, ohne Kürzungen
-            else:
-                print(t[t[col] == filter])
-        except MyCustomError as e:
-            print(f"Caught a custom exception: {e}")
-#--------------------------------------------------------------------------------------------------------------------
-def calculate_differences(puts_df, calls_df):
+def subtractExecutedCallsFromPuts(puts_df, calls_df):
     p = puts_df
     # Konvertiere das Datum/Zeit Feld in ein datetime Objekt für einfachere Verarbeitung
     sd = 'Datum/Zeit'
@@ -177,13 +24,14 @@ def calculate_differences(puts_df, calls_df):
     sm = 'Menge'
     st = 'Ticker'
     
+    p[sd] = p[sd].str.replace(',', '')
     p[sd] = pd.to_datetime(p[sd])
     calls_df[sd] = pd.to_datetime(calls_df[sd])
     # Konvertiere Preis- und Mengen-Spalten von String zu float bzw. int
     p[sp] = p[sp].astype(float)
     p[sm] = p[sm].astype(int)
     calls_df[sp] = calls_df[sp].astype(float)
-    calls_df[sm] = calls_df[sm].astype(int)
+    calls_df[sm] = calls_df[sm].astype(float)
     
     results = []    
     # Iteriere über die Ticker in den Calls
@@ -220,82 +68,43 @@ def calculate_differences(puts_df, calls_df):
     result_df = pd.DataFrame(results)
     return result_df
 #--------------------------------------------------------------------------------------------------------------------
-def showSpecificTable(obj, tableName:str,columns):
-    table = obj.get_table(tableName)
-    if table is not None:
-        if columns:
-            col_range = columns.split('-')
-            start_col = int(col_range[0])
-            end_col = int(col_range[1]) + 1
-            table = table.iloc[:, start_col:end_col]
-        print(f"Tabelle: {tableName}")
-        print(table.to_string())
-    else:
-        print(f"Tabelle '{tableName}' nicht gefunden.")
-#--------------------------------------------------------------------------------------------------------------------
-def showTableColumn(prefix,table, frm, to):
-    print(prefix,end=":")
-    for i in range(frm,to):
-        print(table.iloc[i],end=",")
-    print()
-#--------------------------------------------------------------------------------------------------------------------
-def getExecutedShorts(obj):
-    obj.getSoldOptions()
-    executedShorts = obj.extractExecutedShorts()
-    p = obj.getExecutedPuts()
-    c = obj.getExecutedCalls()
+def getExecutedShorts(tables):
+    t=filter.soldCallsPuts(tables)
+    r=filter.soldOptionsValues(t)
+    print(t.to_string(), "\n", r.to_string())
+    executedShorts = filter.executedOptions(tables)
+    p = filter.executedPuts(executedShorts)
+    c = filter.executedCalls(executedShorts)
     return executedShorts, p,c
 #--------------------------------------------------------------------------------------------------------------------
-def showTaxRelevantTables(obj):
-    print("-"*100)
-    table_name = 'Übersicht  zur realisierten und unrealisierten Performance'
-    obj.filterTable(table_name,'Vermögenswertkategorie','Gesamt',7)
-    obj.delCols(['Symbol', 'Kostenanp.'])
-    # rename
-    r = obj.getNameResult()[1]['Vermögenswertkategorie']
-    for i, item in enumerate(['Gesamt Aktien','Gesamt Optionen','Gesamt Devisen']):
-        r.iat[i] = item
-    obj.displayLastExecutionResult()
-    print("-"*100)
-    obj.filterTable('Zinsen','Währung','Gesamt*',4)
-    obj.delCols(['Datum', 'Beschreibung'])
-    obj.displayLastExecutionResult("Währung","Gesamt Zinsen in EUR")
-    print("-"*100)
-    obj.filterTable('Quellensteuer','Währung','Gesamt*',5)
-    obj.delCols(['Datum', 'Beschreibung', 'Code'])
-    obj.displayLastExecutionResult('Währung',"Gesamt Quellensteuer in EUR")
-    print("-"*100)
-    obj.filterTable('Dividenden','Währung','Gesamt*',5)
-    obj.delCols(['Datum', 'Beschreibung', 'Code'])
-    obj.displayLastExecutionResult('Währung',"Gesamtwert in EUR")
-    print("-"*100)
+def showTaxRelevantTables(tables):
+    dp.showPerformance(*filter.tablePerformance(tables))
+    dp.showZinsen(*filter.tableZinsen(tables))
+    dp.showQuellensteuer(*filter.tableQuellensteuer(tables))
+    dp.showDividenden(*filter.tableDividenden(tables))
 #--------------------------------------------------------------------------------------------------------------------
-def showCorrectedCalculation(obj):
-    print("-"*100)
-    executedShorts,p,c = getExecutedShorts(obj)
-    print("-"*100)
-    #print("\nExecuted Shorts\n", executedShorts.to_string(na_rep='-'))
-    print("\nAusgeführte Puts\n" , p.to_string(na_rep='-'))
-    print("\nAusgeführte Calls\n", c.to_string(na_rep='-'))
-    print()
+def showCorrectedCalculation(tables):
+    stocksStart = filter.tableStocksStart(tables)
+    m = None
+    try:
+        stocksStart = db.loadStockFromYearBefore("stocksbefore.csv")
+    except:
+        pass
 
-    putsMinusCalls = calculate_differences(p, c)
-    print("Calls vs puts:\n", putsMinusCalls.to_string(na_rep='-'))
-    if 1:
-        df = putsMinusCalls
-        col= "Preis_Differenz"
-        plus = df[df[col] > 0][col].sum()
-        minus = df[df[col] < 0][col].sum()
-        print("-"*100)
-        print("Aktien Gewinn:", 100*plus)
-        print("Aktien Verlust:", 100*minus)
-        print("-"*100)
-
-    if debug: print("Übrig gebliebene puts:\n", p.to_string(na_rep='-'))
-    p.drop('Bis', axis=1, inplace=True)    
+    executedShorts,p,c = getExecutedShorts(tables)
+    if globals.debug: dp.showExecutedShorts(executedShorts)
+    dp.showStartStocks(stocksStart)
+    stocksStart["Menge"] /= 100
+    dp.showExecutedPuts(p)
+    dp.showExecutedCalls(c)
+    p = filter.mergePutsAndStocks(stocksStart,p)
+    putsMinusCalls = subtractExecutedCallsFromPuts(p, c)
+    dp.showPutsVsCalls(putsMinusCalls)
+    filter.tableRemainingExecutedPuts(p)
+    p = p[p["Menge"]>0]
     p["Menge"] *= 100
-    print("Bestehende Aktien:\n", p[p["Menge"]>0].to_string(na_rep='-'))
-    print("-"*100)
+    dp.showRemainingStocks(p)
+    db.saveRemainingStocks("stocksafter.csv", p)    
 #--------------------------------------------------------------------------------------------------------------------
 def parseArguments():
     parser = argparse.ArgumentParser(description='Lade eine IBKR/CAPTRADER CSV Datei und erstelle Daraus separate Tabellen.')
@@ -316,22 +125,23 @@ def main():
     filename = args.file_path
 
     if args.encoding != "UTF-8-SIG":    
-        args.encoding = getEncoding(filename)
+        args.encoding = al.getEncoding(filename)
     if args.align:
-        alignSeparators(args.file_path, args.align, args.encoding)
+        al.alignSeparators(args.file_path, args.align, args.encoding)
         filename = args.align
 
-    processor = CSVTableProcessor(filename, delimiter=args.delimiter, decimal=args.decimal, encoding=args.encoding)
+    tables = db.CSVTableProcessor(filename, delimiter=args.delimiter, decimal=args.decimal, encoding=args.encoding).getTables()
+    filter.setTables(tables)
     if args.list:
-        availableTables = list(processor.tables.keys())
+        availableTables = list(tables.keys())
         print("Verfügbare Tabellen:", availableTables)
+    if args.table:
+        dp.showSpecificTable(tables,args.table,args.columns)
 
     if args.tax:
-        showTaxRelevantTables(processor)
+        showTaxRelevantTables(tables)
     if args.new:
-        showCorrectedCalculation(processor)
-    if args.table:
-        showSpecificTable(processor,args.table,args.columns)
+        showCorrectedCalculation(tables)
 #--------------------------------------------------------------------------------------------------------------------
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
